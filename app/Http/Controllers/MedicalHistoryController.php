5b27bf2787ebb140d8d5353a0f69043b598805fc
laefy13 <karloosss13@gmail.com>
2024-02-02 00:02:41 +0800

    medhist update

--- app/Http/Controllers/MedicalHistoryController.php
+++ app/Http/Controllers/MedicalHistoryController.php
@@ -21,21 +21,18 @@ class MedicalHistoryController extends Controller
     }
 
     public function store(Request $request){
-        $med_his = new MedicalHistory ; 
-        $med_his->medhis_ptnt_id = $request->medhis_ptnt_id;
-        $med_his->medhis_record_date = $request->medhis_record_date;
-        $med_his->medhis_record_time = $request->medhis_record_time;
-        $med_his->medhis_height = $request->medhis_height;
-        $med_his->medhis_weight = $request->medhis_weight;
-        $med_his->medhis_blood_pressure = $request->medhis_blood_pressure;
-        $med_his->medhis_glucose = $request->medhis_glucose;
-        $med_his->medhis_illness = $request->medhis_illness;
-
-        $med_his->save();
-        return response()->json([
-            "message" => "Medical History added"
-        ],201);
-
+        $med_his = MedicalHistory::create([
+            'medhis_ptnt_id' => $request->medhis_ptnt_id,
+            'medhis_record_date' => Carbon::parse($request->medhis_record_date)->format('Y-m-d'),
+            'medhis_record_time' => Carbon::parse($request->medhis_record_time)->format('H:i:s'),
+            'medhis_height' => $request->medhis_height,
+            'medhis_weight' => $request->medhis_weight,
+            'medhis_blood_pressure' => $request->medhis_blood_pressure,
+            'medhis_glucose' => $request->medhis_glucose,
+            'medhis_illness' => $request->medhis_illness,
+        ]);
+        
+        return response()->json(["message" => "Medical History added"], 201);
     }
 
     public function update(Request $request){
