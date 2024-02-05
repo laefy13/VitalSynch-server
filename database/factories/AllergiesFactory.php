6c82ccce5b0e32f9e69114585e1cf9597e40ca23
laefy13 <karloosss13@gmail.com>
2024-02-05 17:40:11 +0800

    no.vendor

--- database/factories/AllergiesFactory.php
+++ database/factories/AllergiesFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Factory as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Allergies>
