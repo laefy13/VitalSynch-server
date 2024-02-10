5f7f1a4fa545af6c09c264c9ebf50af4ced9832a
laefy13 <karloosss13@gmail.com>
2024-02-10 15:43:04 +0800

    a

--- database/migrations/2023_12_08_161333_create_guardian_profiles_table.php
+++ database/migrations/2023_12_08_161333_create_guardian_profiles_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 return new class extends Migration
 {
@@ -12,7 +14,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tbl_grdn_profile', function (Blueprint $table) {
-            $table->increments('grdn_id');
+            $table->increments('grdn_key');
+            $table->string('grdn_id');
             $table->string('grdn_surname');
             $table->string('grdn_first_name');
             $table->string('grdn_mid_name');
@@ -24,6 +27,18 @@ return new class extends Migration
             $table->string('grdn_contact_number');
             $table->string('grdn_address');
         });
+        DB::table('tbl_grdn_profile')->insert([
+
+            'grdn_surname' => 'Smith',
+            'grdn_first_name' => 'John',
+            'grdn_mid_name' => 'Doe',
+            'grdn_sex' => 'Male',
+            'grdn_birth_date' => '1990-01-01',
+            'grdn_blood_group' => 'O',
+            'grdn_marital_status' => 'Single',
+            'grdn_contact_number' => '1234567890',
+            'grdn_address' => '123 Main Street, City',
+        ]);
     }
 
     /**
